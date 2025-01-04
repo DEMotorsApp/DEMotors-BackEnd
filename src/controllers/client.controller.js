025da93b809de7041e8c5992a61ad877c0740a7c
Fernando Tumax <ftumax@guaitech.com>
2025-01-04 17:55:55 -0600

    Modificaciones en query para la creacion de reportes de ordenes de servicio

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -45,23 +45,36 @@ exports.getReportClientsEquipment = async (req, res) => {
         .input('start_date', sql.VarChar, startDate)
         .input('end_date', sql.VarChar, endDate)
         .query(`
-            SELECT
-                cc.FULL_NAME as CLIENT,
-                cc.ADDRESS_CLIENT,
-                ces.DESCRIPTION_SERIE as NO_SERIE,
-                FORMAT(cc.ENTRY_DATE, 'dd/MM/yyyy') AS DATE,
-                CASE
-                    WHEN pso.WORK_DONE = 1 THEN 'Trabajo Realizado'
-                    ELSE 'Trabajo No Realizado'
-                END AS WORK_DONE
-            FROM DB_DEMOTORS_DESARROLLO.dbo.PRO_SERVICE_ORDER pso
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_CLIENT cc ON cc.ID_CLIENT =  pso.ID_CLIENT
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT ce ON ce.ID_EQUIPMENT = pso.ID_EQUIPMENT
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT_SERIE ces on ces.ID_SERIE = ce.ID_SERIE
-            WHERE cc.ID_CLIENT = @id_client
-            AND cc.ENTRY_DATE BETWEEN CONVERT(datetime, @start_date, 21) AND CONVERT(datetime, @end_date, 21)    
+            SELECT (
+                SELECT
+                    cc.FULL_NAME as CLIENT,
+                    cc.ADDRESS_CLIENT,
+                    ces.DESCRIPTION_SERIE as NO_SERIE,
+                    FORMAT(cc.ENTRY_DATE, 'dd/MM/yyyy') AS DATE,
+                    CASE
+                        WHEN pso.WORK_DONE = 1 THEN 'Trabajo Realizado'
+                        ELSE 'Trabajo No Realizado'
+                    END AS WORK_DONE,
+                    CASE
+                        WHEN pso.NO_ORDER IS NULL THEN 'No Existe'
+                        ELSE pso.NO_ORDER
+                    END AS NO_ORDER
+                FROM DB_DEMOTORS_DESARROLLO.dbo.PRO_SERVICE_ORDER pso
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_CLIENT cc ON cc.ID_CLIENT =  pso.ID_CLIENT
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT ce ON ce.ID_EQUIPMENT = pso.ID_EQUIPMENT
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT_SERIE ces on ces.ID_SERIE = ce.ID_SERIE
+                WHERE cc.ID_CLIENT = @id_client
+                AND cc.ENTRY_DATE BETWEEN CONVERT(datetime, @start_date, 21) AND CONVERT(datetime, @end_date, 21)
+                FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER
+            ) AS results
         `)
-        res.status(200).json({ response: result.recordset })
+
+        const data  = result.recordset.map((item) => {
+                console.log('item => ', item)
+                return JSON.parse(item.results)
+        })
+
+        res.status(200).json({ response: data })
     } catch (err) {
         res.status(500).json({
             status: 'ERROR',
--- src/controllers/servicesOrder.controller.js
+++ src/controllers/servicesOrder.controller.js
@@ -3,37 +3,44 @@ const config = require('../../configs/sqlServerConfig')
 
 exports.getServicesOrders = async (req, res) => {
     const { idClient, startDate, endDate } = req.params
-    try 
-    {
+    try {
         console.log(idClient, startDate, endDate)
         const pool = await sql.connect(config);
         const result = await pool.request()
-        .input('id_client', sql.Int, idClient)
-        .input('start_date', sql.VarChar, startDate)
-        .input('end_date', sql.VarChar, endDate)
-        .query(`
-            SELECT
-                pso.NO_ORDER,
-                cc.FULL_NAME as CLIENT,
-                cc.ADDRESS_CLIENT,
-                ces.DESCRIPTION_SERIE as NO_SERIE,
-                FORMAT(cc.ENTRY_DATE, 'dd/MM/yyyy') AS DATE,
-                CASE
-                    WHEN pso.WORK_DONE = 1 THEN 'Trabajo Realizado'
-                    ELSE 'Trabajo No Realizado'
-                END AS WORK_DONE
-            FROM DB_DEMOTORS_DESARROLLO.dbo.PRO_SERVICE_ORDER pso
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_CLIENT cc ON cc.ID_CLIENT =  pso.ID_CLIENT
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT ce ON ce.ID_EQUIPMENT = pso.ID_EQUIPMENT
-            INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT_SERIE ces on ces.ID_SERIE = ce.ID_SERIE
-            WHERE cc.ID_CLIENT = @id_client
-            AND cc.ENTRY_DATE BETWEEN CONVERT(datetime, @start_date, 21) AND CONVERT(datetime, @end_date, 21)  
+            .input('id_client', sql.Int, idClient)
+            .input('start_date', sql.VarChar, startDate)
+            .input('end_date', sql.VarChar, endDate)
+            .query(`
+            SELECT (
+                SELECT
+                    cc.FULL_NAME as CLIENT,
+                    cc.ADDRESS_CLIENT,
+                    ces.DESCRIPTION_SERIE as NO_SERIE,
+                    FORMAT(cc.ENTRY_DATE, 'dd/MM/yyyy') AS DATE,
+                    CASE
+                        WHEN pso.WORK_DONE = 1 THEN 'Trabajo Realizado'
+                        ELSE 'Trabajo No Realizado'
+                    END AS WORK_DONE,
+                    CASE
+                        WHEN pso.NO_ORDER IS NULL THEN 'No Existe'
+                        ELSE pso.NO_ORDER
+                    END AS NO_ORDER
+                FROM DB_DEMOTORS_DESARROLLO.dbo.PRO_SERVICE_ORDER pso
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_CLIENT cc ON cc.ID_CLIENT =  pso.ID_CLIENT
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT ce ON ce.ID_EQUIPMENT = pso.ID_EQUIPMENT
+                INNER JOIN DB_DEMOTORS_DESARROLLO.dbo.CAT_EQUIPMENT_SERIE ces on ces.ID_SERIE = ce.ID_SERIE
+                WHERE cc.ID_CLIENT = @id_client
+                AND cc.ENTRY_DATE BETWEEN CONVERT(datetime, @start_date, 21) AND CONVERT(datetime, @end_date, 21) 
+                FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER
+            ) AS results
         `);
-        console.log(result.recordset)
-        res.status(200).json({response : result.recordset});
+        const data = result.recordset.map((item) => {
+            console.log('item => ', item)
+            return JSON.parse(item.results)
+        })
+        res.status(200).json({ response: data });
     }
-    catch (err) 
-    {
+    catch (err) {
         res.status(500).json({
             status: 'ERROR',
             message: `Error al obtener Ordenes de servicio. ${err}`
@@ -49,10 +56,10 @@ exports.createServicesOrder = async (req, res) => {
         const result = await pool.request()
             .input('jsonInput', sql.NVarChar(sql.MAX), JSON.stringify(jsonInput))
             .execute('sp_insert_services_order')
-            console.log(result)
-            const response = result.recordset[0]
-            const { JsonResponse } = response
-            res.status(200).json({ response: JSON.parse(JsonResponse) })
+        console.log(result)
+        const response = result.recordset[0]
+        const { JsonResponse } = response
+        res.status(200).json({ response: JSON.parse(JsonResponse) })
     } catch (err) {
         console.log(err)
         res.status(500).json({
